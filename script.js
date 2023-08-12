5d1eb302022167668a675468ab4d6c5d082d7507
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-12 14:47:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,19 @@ function resetGame() {
   foodPosition = getRandomPosition();
   document.getElementById("startButton").disabled = false;
 }
+document.getElementById('upButton').addEventListener('click', () => {
+  if (direction !== 'down') direction = 'up';
+});
+
+document.getElementById('downButton').addEventListener('click', () => {
+  if (direction !== 'up') direction = 'down';
+});
+
+document.getElementById('leftButton').addEventListener('click', () => {
+  if (direction !== 'right') direction = 'left';
+});
+
+document.getElementById('rightButton').addEventListener('click', () => {
+  if (direction !== 'left') direction = 'right';
+});
+
