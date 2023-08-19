bc3e960e31b1dbaa1e396507a284f88794bb861f
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-18 22:33:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,81 @@ function updateGame() {
     renderGame();
 }
 
-// ... (restante do seu código JavaScript)
+function moveSnake() {
+    const newHead = { ...snakeSegments[0] };
 
+    switch (direction) {
+        case 'right':
+            newHead.x++;
+            break;
+        case 'left':
+            newHead.x--;
+            break;
+        case 'up':
+            newHead.y--;
+            break;
+        case 'down':
+            newHead.y++;
+            break;
+    }
 
+    snakeSegments.unshift(newHead);
+
+    if (newHead.x === foodPosition.x && newHead.y === foodPosition.y) {
+        foodPosition = getRandomPosition();
+    } else {
+        snakeSegments.pop();
+    }
+}
+
+function checkCollision() {
+    const head = snakeSegments[0];
+    if (head.x < 0 || head.x >= 20 || head.y < 0 || head.y >= 20) {
+        endGame();
+    }
+    
+    for (let i = 1; i < snakeSegments.length; i++) {
+        if (head.x === snakeSegments[i].x && head.y === snakeSegments[i].y) {
+            endGame();
+        }
+    }
+}
+
+function renderGame() {
+    gameContainer.innerHTML = '';
+
+    snakeSegments.forEach(segment => {
+        const snakeElem = document.createElement('div');
+        snakeElem.style.gridColumnStart = segment.x + 1;
+        snakeElem.style.gridRowStart = segment.y + 1;
+        snakeElem.classList.add('snake');
+        gameContainer.appendChild(snakeElem);
+    });
+
+    const foodElem = document.createElement('div');
+    foodElem.style.gridColumnStart = foodPosition.x + 1;
+    foodElem.style.gridRowStart = foodPosition.y + 1;
+    foodElem.classList.add('food');
+    gameContainer.appendChild(foodElem);
+}
+
+function getRandomPosition() {
+    return {
+        x: Math.floor(Math.random() * 20),
+        y: Math.floor(Math.random() * 20)
+    };
+}
+
+function endGame() {
+    clearInterval(gameInterval);
+    alert('Game Over!');
+    resetGame();
+    renderGame();
+}
+
+function resetGame() {
+    snakeSegments = [{ x: 10, y: 10 }];
+    direction = 'right';
+    foodPosition = getRandomPosition();
+    document.getElementById('startButton').disabled = false;
+}
