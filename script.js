357175cae39727514e0ab352864295da366d56e0
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-20 12:40:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const gameContainer = document.querySelector(".game-container");
 let snake = [{ x: 10, y: 10 }];
 let direction = { x: 1, y: 0 };
-let food = { x: 5, y: 5 };
+let food = placeFoodInsideGrid();
 let gameInterval;
 let foodCollectedCount = 0;
-let placeFoodOutside = false;
 
 document.getElementById("startButton").addEventListener("click", startGame);
 document.getElementById("restartButton").addEventListener("click", restartGame);
@@ -18,9 +17,8 @@ function restartGame() {
   clearInterval(gameInterval);
   snake = [{ x: 10, y: 10 }];
   direction = { x: 1, y: 0 };
-  food = { x: 5, y: 5 };
+  food = placeFoodInsideGrid();
   foodCollectedCount = 0;
-  placeFoodOutside = false;
   document.getElementById("gameOver").style.display = "none";
   startGame();
 }
@@ -60,36 +58,35 @@ function checkGameOver() {
 function checkFoodCollision() {
   if (snake[0].x === food.x && snake[0].y === food.y) {
     foodCollectedCount++;
-
     if (foodCollectedCount % 10 === 0) {
-      placeFoodOutside = true;
-    }
-
-    if (placeFoodOutside) {
-      const side = Math.floor(Math.random() * 4);
-      switch (side) {
-        case 0: // left
-          food = { x: -2, y: Math.floor(Math.random() * 20) };
-          break;
-        case 1: // right
-          food = { x: 21, y: Math.floor(Math.random() * 20) };
-          break;
-        case 2: // top
-          food = { x: Math.floor(Math.random() * 20), y: -2 };
-          break;
-        case 3: // bottom
-          food = { x: Math.floor(Math.random() * 20), y: 21 };
-          break;
-      }
-      placeFoodOutside = false;
+      food = placeFoodOutsideGrid();
     } else {
-      food = {
-        x: Math.floor(Math.random() * 20),
-        y: Math.floor(Math.random() * 20),
-      };
+      food = placeFoodInsideGrid();
     }
-
     snake.push({ ...snake[snake.length - 1] });
+  }
+}
+
+function placeFoodInsideGrid() {
+  return {
+    x: Math.floor(Math.random() * 20),
+    y: Math.floor(Math.random() * 20),
+  };
+}
+
+function placeFoodOutsideGrid() {
+  const side = Math.floor(Math.random() * 4);
+  switch (side) {
+    case 0: // left
+      return { x: -1, y: Math.floor(Math.random() * 20) };
+    case 1: // right
+      return { x: 20, y: Math.floor(Math.random() * 20) };
+    case 2: // top
+      return { x: Math.floor(Math.random() * 20), y: -1 };
+    case 3: // bottom
+      return { x: Math.floor(Math.random() * 20), y: 20 };
+    default:
+      return placeFoodInsideGrid();
   }
 }
 
@@ -103,13 +100,11 @@ function drawGame() {
     gameContainer.appendChild(div);
   }
 
-  if ((food.x >= -2 && food.x <= 21) && (food.y >= -2 && food.y <= 21)) {
-    const foodDiv = document.createElement("div");
-    foodDiv.style.gridRowStart = food.y + 1;
-    foodDiv.style.gridColumnStart = food.x + 1;
-    foodDiv.classList.add("food");
-    gameContainer.appendChild(foodDiv);
-  }
+  const foodDiv = document.createElement("div");
+  foodDiv.style.gridRowStart = food.y + 1;
+  foodDiv.style.gridColumnStart = food.x + 1;
+  foodDiv.classList.add("food");
+  gameContainer.appendChild(foodDiv);
 }
 
 document.addEventListener("keydown", (e) => {
@@ -128,3 +123,4 @@ document.addEventListener("keydown", (e) => {
       break;
   }
 });
+
