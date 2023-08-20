32232ea64584345f89a22b7a895d91b77f5a28d0
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-08-20 12:04:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let snake = [{ x: 10, y: 10 }];
 let direction = { x: 1, y: 0 };
 let food = { x: 5, y: 5 };
 let gameInterval;
+let foodCollectedCount = 0;  // Variável para contar as coletas de comida
 
 document.getElementById("startButton").addEventListener("click", startGame);
 document.getElementById("restartButton").addEventListener("click", restartGame);
@@ -17,8 +18,9 @@ function restartGame() {
   snake = [{ x: 10, y: 10 }];
   direction = { x: 1, y: 0 };
   food = { x: 5, y: 5 };
+  foodCollectedCount = 0;  // Resetar a contagem ao reiniciar o jogo
   document.getElementById("gameOver").style.display = "none";
-  startGame(); // Esta linha foi adicionada.
+  startGame();
 }
 
 function main() {
@@ -55,16 +57,23 @@ function checkGameOver() {
 
 function checkFoodCollision() {
   if (snake[0].x === food.x && snake[0].y === food.y) {
-    food = {
-      x: Math.floor(Math.random() * 20),
-      y: Math.floor(Math.random() * 20),
-    };
+    foodCollectedCount++;
+
+    if (foodCollectedCount % 3 === 0) {
+      food = { x: -1, y: -1 };  // Posiciona a comida fora do grid
+    } else {
+      food = {
+        x: Math.floor(Math.random() * 20),
+        y: Math.floor(Math.random() * 20),
+      };
+    }
     snake.push({ ...snake[snake.length - 1] });
   }
 }
 
 function drawGame() {
   gameContainer.innerHTML = "";
+
   for (let segment of snake) {
     const div = document.createElement("div");
     div.style.gridRowStart = segment.y + 1;
@@ -73,11 +82,14 @@ function drawGame() {
     gameContainer.appendChild(div);
   }
 
-  const foodDiv = document.createElement("div");
-  foodDiv.style.gridRowStart = food.y + 1;
-  foodDiv.style.gridColumnStart = food.x + 1;
-  foodDiv.classList.add("food");
-  gameContainer.appendChild(foodDiv);
+  // Apenas desenhe a comida se estiver dentro dos limites
+  if (food.x >= 0 && food.y >= 0) {
+    const foodDiv = document.createElement("div");
+    foodDiv.style.gridRowStart = food.y + 1;
+    foodDiv.style.gridColumnStart = food.x + 1;
+    foodDiv.classList.add("food");
+    gameContainer.appendChild(foodDiv);
+  }
 }
 
 document.addEventListener("keydown", (e) => {
